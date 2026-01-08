e72df970c4a83c0e8a410de2b33385a979b0c9d4
Vir-xx <dsa.forsure@gmail.com>
2026-01-08 16:40:13 +0530

    commit

--- encryptDecrypt.js
+++ encryptDecrypt.js
@@ -1,23 +1,22 @@
 const CryptoJS = require("crypto-js");
 const fs = require("fs");
 
-// Secret key (later this will be encrypted using public key)
 const SECRET_KEY = "my-very-strong-secret-key";
 
-// Read file
+
 const fileData = fs.readFileSync("sample.txt", "utf8");
 
-// Encrypt
+
 const encrypted = CryptoJS.AES.encrypt(fileData, SECRET_KEY).toString();
 
-// Save encrypted file
+
 fs.writeFileSync("encrypted.txt", encrypted);
 
-// Decrypt
+
 const decryptedBytes = CryptoJS.AES.decrypt(encrypted, SECRET_KEY);
 const decryptedText = decryptedBytes.toString(CryptoJS.enc.Utf8);
 
-// Save decrypted file
+
 fs.writeFileSync("decrypted.txt", decryptedText);
 
 console.log("✅ File encrypted and decrypted successfully");
--- encryption/decryptFile.js
+++ encryption/decryptFile.js
@@ -2,26 +2,26 @@ const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
 
-// Paths
+
 const encryptedFile = path.join(__dirname, "..", "encrypted.bin");
 const outputFile = path.join(__dirname, "..", "decrypted.pdf");
 const keyDir = path.join(__dirname, "keys");
 
-// Read key and IV
+
 const aesKey = fs.readFileSync(path.join(keyDir, "aes.key"));
 const iv = fs.readFileSync(path.join(keyDir, "iv.key"));
 
-// Read encrypted file
+
 const encryptedBuffer = fs.readFileSync(encryptedFile);
 
-// Decrypt
+
 const decipher = crypto.createDecipheriv("aes-256-cbc", aesKey, iv);
 const decryptedData = Buffer.concat([
   decipher.update(encryptedBuffer),
   decipher.final()
 ]);
 
-// Save decrypted file
+
 fs.writeFileSync(outputFile, decryptedData);
 
 console.log("✅ File decrypted successfully");
